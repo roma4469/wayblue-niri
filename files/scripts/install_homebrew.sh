f0123ba2619a5cf8d0d1c624c6036b80bdd9f8f5
roma <roma@sixtyten>
2026-04-19 02:37:51 -0700

    adding support for homebrew

---
+++ files/scripts/install_homebrew.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+# echo 'This is an example shell script'
+# echo 'Scripts here will run during build if specified in recipe.yml'
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
